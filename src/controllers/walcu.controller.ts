c4b57fcbf2c5533cef50b62453f9772ec6308dc6
Roberto Serrano <robertoserrano83@gmail.com>
2022-02-28 14:45:00 +0100

    Nuevo controlador para ventas y test de la firma de Walcu

--- src/controllers/walcu.controller.ts
+++ src/controllers/walcu.controller.ts
@@ -1,5 +1,11 @@
 import { Request, Response } from 'express'
+import crypto, { BinaryLike } from 'crypto'
 
+declare module 'http' {
+  interface IncomingHttpHeaders {
+    'x-walcu-webhook-signature': string
+  }
+}
 
 /**
  * Walcu WebHook.
@@ -9,12 +15,33 @@ export const getWebhook = (req: Request, res: Response) => {
   res.status(200).send('Web Services Server')
 }
 
-
 /**
  * Walcu WebHook.
  * @route POST /
  */
-export const postWebhook = (req: Request, res: Response) => {
+export const postWebhookSale = (req: Request, res: Response) => {
+  const { data } = req.body
+
+  const secret_key: BinaryLike = req.headers['x-walcu-webhook-signature']
+  const hmac = crypto.createHmac('sha256', secret_key)
+  hmac.update(`${process.env.URL}/walcu/webhook.${JSON.stringify(data)}`)
+
+  const signature = hmac.digest('base64')
+
+  console.log(req.headers)
+  console.log(signature)
+
+  console.log(data)
+
+  res.status(200).end()
+}
+
+/**
+ * Walcu WebHook Ventas
+ * @route POST /
+ */
+
+export const postWebhookBuy = (req: Request, res: Response) => {
   const { data } = req.body
   console.log(req.headers.host)
   console.log(req.originalUrl)
