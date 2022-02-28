cf26f74bb5d5c5f183a29c673ac08d0a23f5d252
Roberto Serrano <robertoserrano83@gmail.com>
2022-02-28 15:47:04 +0100

    Verificacion firma de Walcu en la ruta de prueba

--- src/routes/walcu.routes.ts
+++ src/routes/walcu.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express'
 import * as walcuController from '../controllers/walcu.controller'
+import { verifySignature } from '../middlewares/authWalcu'
+
 const walcuRoutes = Router()
 
 walcuRoutes.get('/webhook', walcuController.getWebhook)
-walcuRoutes.post('/webhook', walcuController.postWebhookSale)
+walcuRoutes.post('/webhook', [verifySignature], walcuController.postWebhookSale)
 walcuRoutes.post('/compras/', walcuController.postWebhookBuy)
 
 
