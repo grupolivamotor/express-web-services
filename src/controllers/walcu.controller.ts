c564185d2d23b13d403ea04ce9c33f13807b82a9
Roberto Serrano <robertoserrano83@gmail.com>
2022-02-28 15:47:26 +0100

    Refactor

--- src/controllers/walcu.controller.ts
+++ src/controllers/walcu.controller.ts
@@ -1,12 +1,4 @@
-import 'dotenv/config'
 import { Request, Response } from 'express'
-import crypto, { BinaryLike } from 'crypto'
-
-declare module 'http' {
-  interface IncomingHttpHeaders {
-    'x-walcu-webhook-signature': string
-  }
-}
 
 /**
  * Walcu WebHook.
@@ -23,18 +15,7 @@ export const getWebhook = (req: Request, res: Response) => {
 export const postWebhookSale = (req: Request, res: Response) => {
   const data = req.body
 
-  const secret_key: BinaryLike = req.headers['x-walcu-webhook-signature']
-  const hmac = crypto.createHmac('sha256', secret_key)
-  hmac.update(`${process.env.URL}/walcu/webhook.${JSON.stringify(data)}`)
-
-  const signature = hmac.digest('base64')
-
-  console.log(req.headers)
-  console.log(signature)
-
-  console.log(data)
-
-  res.status(200).end()
+  res.status(200).json(data)
 }
 
 /**
