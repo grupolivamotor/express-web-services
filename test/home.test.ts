d5c141bfbb307d3c965de474a2dde2f59010ac4f
Roberto Serrano <robertoserrano83@gmail.com>
2022-03-04 17:25:15 +0100

    test: GET /

---
+++ test/home.test.ts
@@ -0,0 +1,12 @@
+import request from 'supertest'
+import app from '../src/app'
+
+describe('GET /', () => {
+  it('should return 200 OK', (done) => {
+    request(app).get('/').expect(200, done)
+  })
+
+  it('should return 404 OK', (done) => {
+    request(app).get('/any').expect(404, done)
+  })
+})
