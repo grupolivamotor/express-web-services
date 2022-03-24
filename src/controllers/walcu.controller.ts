74ad89f1a09bdac7fb60979a96ed95e9aea45bde
Roberto Serrano <robertoserrano83@gmail.com>
2022-03-24 11:59:18 +0100

    Comment logs

--- src/controllers/walcu.controller.ts
+++ src/controllers/walcu.controller.ts
@@ -7,7 +7,7 @@ import { Request, Response } from 'express'
 
 export const postWebhookSale = (req: Request, res: Response) => {
   const data = req.body
-  console.log(data)
+  // console.log(data)
   res.status(200).json(data)
 }
 
@@ -18,6 +18,6 @@ export const postWebhookSale = (req: Request, res: Response) => {
 
 export const postWebhookAppraisals = (req: Request, res: Response) => {
   const data = req.body
-  console.log(data)
+  // console.log(data)
   res.status(200).json(data)
 }
