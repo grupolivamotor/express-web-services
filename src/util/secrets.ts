8341602d6bf57b1d25a6a2d05a64fa3214798e35
Roberto Serrano <robertoserrano83@gmail.com>
2022-03-24 11:58:56 +0100

    Credentials for Kulteo API

--- src/util/secrets.ts
+++ src/util/secrets.ts
@@ -5,3 +5,7 @@ export const ENVIRONMENT = process.env.NODE_ENV
 
 export const WALCU_WEBHOOK_SALES = process.env.WALCU_WEBHOOK_SALES
 export const WALCU_WEBHOOK_APPRAISALS = process.env.WALCU_WEBHOOK_APPRAISALS
+
+export const KULTEO_API_URL = process.env.KULTEO_API_URL
+export const KULTEO_API_USERNAME = process.env.KULTEO_API_USERNAME
+export const KULTEO_API_PASS= process.env.KULTEO_API_PASS
