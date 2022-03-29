d6904dd7d4e4f21aa5eb52eb088aa76f92330741
Roberto Serrano <robertoserrano83@gmail.com>
2022-03-29 16:28:41 +0200

    post for papyrum

--- src/controllers/walcu.controller.ts
+++ src/controllers/walcu.controller.ts
@@ -1,14 +1,26 @@
 import { Request, Response } from 'express'
+import { Appraisal, Sale } from '../types'
+import { KULTEO_API_URL, KULTEO_API_USERNAME, KULTEO_API_PASS } from '../util/secrets'
+import axios from 'axios'
 
 /**
  * Walcu WebHook.
  * @route POST /
  */
 
-export const postWebhookSale = (req: Request, res: Response) => {
-  const data = req.body
-  // console.log(data)
-  res.status(200).json(data)
+export const postWebhookSale = async (req: Request, res: Response) => {
+  const data = req.body as unknown as Sale
+
+  const result = await axios.post(`${KULTEO_API_URL}/sales`, data, {
+    auth: {
+      username: KULTEO_API_USERNAME,
+      password: KULTEO_API_PASS
+    }
+  })
+
+  console.log(result)
+
+  res.status(200).json(result)
 }
 
 /**
@@ -17,7 +29,16 @@ export const postWebhookSale = (req: Request, res: Response) => {
  */
 
 export const postWebhookAppraisals = (req: Request, res: Response) => {
-  const data = req.body
-  // console.log(data)
-  res.status(200).json(data)
+  const data = req.body as unknown as Appraisal
+
+  const result = axios.post(`${KULTEO_API_URL}/appraisals`, data, {
+    auth: {
+      username: KULTEO_API_USERNAME,
+      password: KULTEO_API_PASS,
+    },
+  })
+
+  console.log(result)
+  
+  res.status(200).json(result)
 }
